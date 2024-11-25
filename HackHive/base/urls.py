aece90d776ed7af1760d21e1d0e9b1aaa8442640
Ivn Nyaga Muchiri <41712ivnnyagaitl6s023g1@gmail.com>
2024-11-25 20:06:39 +0300

    second commit

--- HackHive/base/urls.py
+++ HackHive/base/urls.py
@@ -1,19 +1,21 @@
+from unicodedata import name
 from django.urls import path
-from . import views
-
+from . import views 
 
 urlpatterns = [
-    path('', views.home_page, name="home"),  # Home page view
+    path('', views.home_page, name="home"),
 
     path('login/', views.login_page, name="login"),
     path('register/', views.register_page, name="register"),
     path('logout/', views.logout_user, name="logout"),
-    path('user/<str:pk>/', views.user_page, name="profile"),  # User profile view
-    path('event/<str:pk>/', views.event_page, name="event"),  # Event detail view
-    path('registration-confirmation/<str:pk>/', views.registration_confirmation, name="registration-confirmation"),  # Event confirmation view
-    
-    path('account/', views.account_page, name="account"),  # Account page view
-    path('project-submission/<str:pk>/', views.project_submission, name="project-submission"),  # Project submission view
+    path('user/<str:pk>/', views.user_page, name="profile"),
+    path('event/<str:pk>/', views.event_page, name="event"),
+    path('registration-confirmation/<str:pk>/', views.registration_confirmation, name="registration-confirmation"),
 
-    path('update-submission/<str:pk/', views.update_submission, name="update-submission"),
+    path('account/', views.account_page, name="account"),
+    path('edit-account/', views.edit_account, name="edit-account"),
+    path('change-password/', views.change_password, name="change-password"),
+    path('project-submission/<str:pk>/', views.project_submission, name="project-submission"),
+
+    path('update-submission/<str:pk>/', views.update_submission, name="update-submission"),
 ]
